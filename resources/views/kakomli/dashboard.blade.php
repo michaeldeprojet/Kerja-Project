a2ecc9bab8b2ebac88ba8fbf503fdf39f44c3364
michaeldeprojet <michaeldeproject@gmail.com>
2022-02-23 14:31:30 +0700

    update michael

--- resources/views/kakomli/dashboard.blade.php
+++ resources/views/kakomli/dashboard.blade.php
@@ -7,25 +7,26 @@
   </div>
 
   <div class="d-flex justify-content-center">
-      <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-      <div class="card-header text-center">Jumlah Seluruh Siswa</div>
-        <div class="card-body">
-          <p class="card-text text-center">1.500</p>
-        </div>
+
+      <div class="card text-dark bg-light mb-3" style="max-width: 18rem; margin-right:20px;">
+        <div class="card-header text-center">Jumlah Seluruh Siswa</div>
+          <div class="card-body">
+            <p class="card-text text-center">1.500</p>
+          </div>
+      </div>
+
+      <div class="card text-dark bg-light mb-3" style="max-width: 18rem; margin-right:20px;">
+        <div class="card-header text-center">Jumlah Siswa PKL</div>
+          <div class="card-body">
+            <p class="card-text text-center">500</p>
+          </div>
       </div>
 
       <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-      <div class="card-header text-center">Jumlah Siswa PKL</div>
-        <div class="card-body">
-          <p class="card-text text-center">500</p>
-        </div>
-      </div>
-
-      <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-      <div class="card-header text-center">Jumlah Siswa Belum PKL</div>
-        <div class="card-body">
-          <p class="card-text text-center">1000</p>
-        </div>
+        <div class="card-header text-center">Jumlah Siswa Belum PKL</div>
+          <div class="card-body">
+            <p class="card-text text-center">1000</p>
+          </div>
       </div>
   </div>
 
