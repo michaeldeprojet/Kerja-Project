1f4da71b5fec339f671ebed567e130c7dddd0f86
altf4m88 <fadhilmauladhani@gmail.com>
2022-02-27 15:18:00 +0700

    auth, middleware

--- app/Models/Login.php
+++ app/Models/Login.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Login extends Model
+class Login extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'login';
+
+    const ADMIN = 'ADMIN';
+    const KAKOMLI = 'KAKOMLI';
+    const PEMBIMBING = 'PEMBIMBING';
+    const SISWA = 'SISWA';
 }
--- database/migrations/2022_02_27_064207_update_login_table.php
+++ database/migrations/2022_02_27_064207_update_login_table.php
@@ -14,7 +14,7 @@ class UpdateLoginTable extends Migration
     public function up()
     {
         Schema::table('login', function (Blueprint $table) {
-            $table->string('role');
+            $table->string('role')->nullable();
         });
     }
 
