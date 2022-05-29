d523035557f7ff76e4d074321afe50531b58a1c2
Alfarizi2103 <alfariziaries2103@gmail.com>
2022-05-29 15:28:43 +0700

    Fixing eror in presensi

--- database/migrations/2022_03_01_052817_create_tb_daftar_hadir.php
+++ database/migrations/2022_03_01_052817_create_tb_daftar_hadir.php
@@ -21,7 +21,7 @@ class CreateTbDaftarHadir extends Migration
             $table->string('nama_perusahaan');
             $table->string('kompetensi');
             $table->string('keterangan');
-            $table->string('alasan');
+            $table->string('alasan')->nullable();
             $table->foreign('id_siswa')->references('id')->on('tb_siswa');
             $table->timestamps();
         });
